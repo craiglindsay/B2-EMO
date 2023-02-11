4c8b2338c62bbcf4fa8688b7dedaa8380eec484a
Craig Lindsay <clindsay@globys.com>
2023-02-11 11:35:21 -0800

    adding animation routines

--- B2-EMO.ino
+++ B2-EMO.ino
@@ -70,12 +70,15 @@ int StrafeMax = 2000;
 int StrafeStop = 1500;
 int HeadLMin = 1000;
 int HeadLMax = 2000;
+int HeadLCenter = 1500;
 int HeadRMin = 1000;
 int HeadRMax = 2000;
+int HeadRCenter = 1650;
 int HeadZMin = 200;
-int HeadZMax = 2000;
+int HeadZMax = 1925;
 int HeadTMin = 200;
 int HeadTMax = 2800;
+int HeadTCenter = 1400;
 int BodyZ1Min = 1100;
 int BodyZ1Max = 1900;
 int BodyZ2Min = 1000;
@@ -105,10 +108,10 @@ int BLHeightMax = 2400;
 int Drive = 1500;
 int Rotate = 1500;
 int Strafe = 1500;
-int HeadL = 1500;
-int HeadR = 1500;
+int HeadL = HeadLCenter;
+int HeadR = HeadRCenter;
+int HeadT = HeadTCenter;
 int HeadZ = 1200;
-int HeadT = 1500;
 int BodyZ1 = 1500;
 int BodyZ2 = 1500;
 int FLSpread = 1500;
@@ -181,11 +184,17 @@ long previousFootMillis = millis();
 long previousSpeedToggleMillis = millis();
 long currentMillis = millis();
 long footServoMillis = millis();
+
 int serialLatency = 25;  //This is a delay factor in ms to prevent queueing of the Serial data. 25ms seems approprate for HardwareSerial, values of 50ms or larger are needed for Softare Emulation
-int soundNum;            //To be used for incrementing sounds not implement yet
 int speedToggleButtonCounter = 0;
 boolean footServosAttached = false;
 
+unsigned long soundprevmillis = 0;    // storage for sound timer
+unsigned long soundcurrentmillis = 0; // storage for sound timer
+unsigned long soundTimer;             // storage for sound timer
+int soundNum;                         // Sound number
+
+int animate = 0; 
 // ---------------------------------------------------------------------------------------
 // Setup for USB and Bluetooth Devices////////////////////////////
 // ---------------------------------------------------------------------------------------
@@ -225,7 +234,7 @@ void setup()
   mySoftwareSerial.begin(9600);       //Setup Serial for DF player
   myDFPlayer.begin(mySoftwareSerial); //Use softwareSerial to communicate with dfplayer
   myDFPlayer.volume(volume);          //Set dfplayer volume value above
-  myDFPlayer.play(21);                //Play the 21st mp3 as a startup sound
+  myDFPlayer.play(16);                //Play the 21st mp3 as a startup sound
   
   //Setup for serial 3
   Serial3.begin(115200);            //Serial3:: Used for lighting to arduino nano
@@ -246,14 +255,6 @@ ServoHeadZ.attach(PinHeadZ);
 ServoHeadT.attach(PinHeadT);;
 ServoBodyZ1.attach(PinBodyZ1);
 ServoBodyZ2.attach(PinBodyZ2);
-//ServoFLHeight.attach(PinFLHeight);
-//ServoFRHeight.attach(PinFRHeight);
-//ServoBRHeight.attach(PinBRHeight);
-//ServoBLHeight.attach(PinBLHeight);
-//ServoFLSpread.attach(PinFLSpread);
-//ServoFRSpread.attach(PinFRSpread);
-//ServoBRSpread.attach(PinBRSpread);
-//ServoBLSpread.attach(PinBLSpread);
 
 // =======================================================================================
 // Set Initial Servo Positions
@@ -301,6 +302,9 @@ void loop()   	//LOOP through functions from highest to lowest priority.
 	toggleSettings(); // reads the PS3 remote for other button presses
 	footServoDetach(); // Looks to see if it has be 5 seconds since last foot height adjustment
 	currentMillis = millis();
+	if (soundNum >0) { animationSounds(); }
+	if (animate == 1) { animationLow(); }
+	if (animate == 2) { animationHigh(); }
 }
 
 // =======================================================================================
@@ -413,10 +417,10 @@ boolean ps3MotorDrive(PS3BT* myPS3 = PS3Nav)
           //Serial.println(HeadT);
           } 
           else if (myPS3->getButtonPress(L2) && !myPS3->getButtonPress(L1) && !myPS3->getButtonPress(L3))  // L2 and Stick - Head L & R Servo 
-        	 {
-            Serial.print("Head R/L ");
-		        HeadR = HeadR - ((StickY*-1) + (StickX))/2;                     // New method for incrementing with stick movement but staying in place
-		        HeadL = HeadL - ((StickY) + (StickX))/2;                        // New method for incrementing with stick movement but staying in place
+           {
+           Serial.print("Head R/L ");
+		   HeadR = HeadR - ((StickY*-1) + (StickX))/2;                     // New method for incrementing with stick movement but staying in place
+		   HeadL = HeadL - ((StickY) + (StickX))/2;                        // New method for incrementing with stick movement but staying in place
            //HeadR = ((1500 - (12*StickY*-1)) + (1500 + (12*StickX*-1)))/2; // Old method for moving with stick
            //HeadL = ((1500 - (12*StickY)) + (1500 - (12*StickX)))/2;       // Old method for moving with stick
            if (HeadL > HeadLMax) HeadL=HeadLMax;
@@ -552,18 +556,22 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
 	// Circle + Up 
 	if(myPS3->getButtonPress(CIRCLE) && myPS3->getButtonClick(UP))
 	{
+		raiseHead();
 	}
 	// Circle + Down 
 	if(myPS3->getButtonPress(CIRCLE) && myPS3->getButtonClick(DOWN))
 	{
+		lowerHead();
 	}
 	// Circle + Left 
 	if(myPS3->getButtonPress(CIRCLE) && myPS3->getButtonClick(LEFT))
 	{
+		centerHead();
 	}
 	// Circle + Right 
 	if(myPS3->getButtonPress(CIRCLE) && myPS3->getButtonClick(RIGHT))
 	{
+		levelHead();
 	}
 	// Circle + Cross 
 	if(myPS3->getButtonPress(CIRCLE) && myPS3->getButtonClick(CROSS))
@@ -592,11 +600,12 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
 	// Cross + Up - Spin rings
 	if(myPS3->getButtonPress(CROSS) && myPS3->getButtonClick(UP))
 	{
-    digitalWrite(PinUpperMotorA, LOW);
+	digitalWrite(PinUpperMotorA, LOW);
     digitalWrite(PinUpperMotorB, HIGH);
     digitalWrite(PinLowerMotorA, HIGH);
     digitalWrite(PinLowerMotorB, LOW);
-    delay(500);
+    myDFPlayer.play(11);
+	delay(700);
     digitalWrite(PinUpperMotorA, HIGH);
     digitalWrite(PinUpperMotorB, HIGH);
     digitalWrite(PinLowerMotorA, HIGH);
@@ -611,7 +620,8 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
     digitalWrite(PinUpperMotorB, LOW);
     digitalWrite(PinLowerMotorA, LOW);
     digitalWrite(PinLowerMotorB, HIGH);
-    delay(500);
+    myDFPlayer.play(11);
+	delay(700);
     digitalWrite(PinUpperMotorA, HIGH);
     digitalWrite(PinUpperMotorB, HIGH);
     digitalWrite(PinLowerMotorA, HIGH);
@@ -626,7 +636,8 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
     digitalWrite(PinUpperMotorB, HIGH);
     digitalWrite(PinLowerMotorA, LOW);
     digitalWrite(PinLowerMotorB, HIGH);
-    delay(500);
+    myDFPlayer.play(11);
+	delay(700);
     digitalWrite(PinUpperMotorA, HIGH);
     digitalWrite(PinUpperMotorB, HIGH);
     digitalWrite(PinLowerMotorA, HIGH);
@@ -639,7 +650,8 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
     digitalWrite(PinUpperMotorB, LOW);
     digitalWrite(PinLowerMotorA, HIGH);
     digitalWrite(PinLowerMotorB, LOW);
-    delay(500);
+    myDFPlayer.play(11);
+	delay(700);
     digitalWrite(PinUpperMotorA, HIGH);
     digitalWrite(PinUpperMotorB, HIGH);
     digitalWrite(PinLowerMotorA, HIGH);
@@ -687,19 +699,30 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
 // =======================================================================================
 // PS Button Combos 
 // =======================================================================================
-	// PS + Up 
+	// PS + Up - B2 Expands
 	if(myPS3->getButtonPress(PS) && myPS3->getButtonClick(UP))
 	{
-		// Build routine to raise to max height
+		expandBody();
+		raiseHead();
+		levelHead();
+		centerHead();
 	}
-	// PS + Down 
+	// PS + Down  - B2 Tucks-In
 	if(myPS3->getButtonPress(PS) && myPS3->getButtonClick(DOWN))
 	{
-		//Build routine to tuck in
+		levelHead();
+		centerHead();
+		lowerHead();
+		tuckInBody();
+
 	}
-	// PS + Left 
+	// PS + Left - Toggles automatic sounds
 	if(myPS3->getButtonPress(PS) && myPS3->getButtonClick(LEFT))
 	{
+		if (soundNum == 0) {
+			soundNum = 1;
+			soundprevmillis = soundcurrentmillis;
+			} else {soundNum = 0;}
 	}
 	// PS + Right 
 	if(myPS3->getButtonPress(PS) && myPS3->getButtonClick(RIGHT))
@@ -709,9 +732,14 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
 	if(myPS3->getButtonPress(PS) && myPS3->getButtonClick(CIRCLE))
 	{
 	}
-	// PS + Cross -
+	// PS + Cross - Automation routine toggle, Off, Low, High
 	if(myPS3->getButtonPress(PS) && myPS3->getButtonClick(CROSS))
 	{
+			if (animate == 0) {
+			animate = 1;
+			} else if (animate == 1) {
+			animate = 2;
+			} else {animate = 0;}	
 	}
 	// PS + L1 
 	if(!myPS3->getButtonPress(L2) && myPS3->getButtonPress(PS) && myPS3->getButtonClick(L1))
@@ -781,15 +809,12 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
 	// L2 + Up
 	if(!myPS3->getButtonPress(L1) && myPS3->getButtonPress(L2) && myPS3->getButtonClick(UP))
 	{
-          ServoBodyZ1.writeMicroseconds(BodyZ1Max);
-          ServoBodyZ2.writeMicroseconds(BodyZ2Min);
+    expandBody();
 	}
 	// L2 + Down 
 	if(!myPS3->getButtonPress(L1) && myPS3->getButtonPress(L2) && myPS3->getButtonClick(DOWN))
 	{
-
-          ServoBodyZ1.writeMicroseconds(BodyZ1Min);
-          ServoBodyZ2.writeMicroseconds(BodyZ2Max);
+	tuckInBody();
 	}
 	// L2 + Left 
 	if(!myPS3->getButtonPress(L1) && myPS3->getButtonPress(L2) && myPS3->getButtonClick(LEFT))
@@ -871,26 +896,32 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
 	// L1 + Up 
 	if(myPS3->getButtonPress(L1) && myPS3->getButtonClick(UP) && !myPS3->getButtonPress(L2))
 	{
+		myDFPlayer.play(1);
 	}
 	// L1 + Down 
 	if(myPS3->getButtonPress(L1) && myPS3->getButtonClick(DOWN) && !myPS3->getButtonPress(L2))
 	{
+		myDFPlayer.play(2);
 	}
 	// L1 + Left
 	if(myPS3->getButtonPress(L1) && myPS3->getButtonClick(LEFT) && !myPS3->getButtonPress(L2))
 	{
+		myDFPlayer.play(5);
 	}
 	// L1 + Right 
 	if(myPS3->getButtonPress(L1) && myPS3->getButtonClick(RIGHT) && !myPS3->getButtonPress(L2))
 	{
+		myDFPlayer.play(6);
 	}
 	// L1 + Circle 
 	if(myPS3->getButtonPress(L1) && myPS3->getButtonClick(CIRCLE) && !myPS3->getButtonPress(L2))
 	{
+		myDFPlayer.play(10);
 	}
 	// L1 + Cross 
 	if(myPS3->getButtonPress(L1) && myPS3->getButtonClick(CROSS) && !myPS3->getButtonPress(L2))
 	{
+		myDFPlayer.play(13);
 	}
 	// L1 + PS 
 	if(!myPS3->getButtonPress(L2) && myPS3->getButtonPress(L1) && myPS3->getButtonClick(PS))
@@ -906,20 +937,24 @@ void ps3ToggleSettings(PS3BT* myPS3 = PS3Nav)
 // =======================================================================================
 	// Up - 
 	if (PS3Nav->getButtonClick(UP) && !PS3Nav->getButtonPress(CROSS) && !PS3Nav->getButtonPress(CIRCLE) && !PS3Nav->getButtonPress(L1) && !PS3Nav->getButtonPress(L2) && !PS3Nav->getButtonPress(PS)) {
+	myDFPlayer.play(12);
 	}
 	// Down  - 
 	if (PS3Nav->getButtonClick(DOWN) && !PS3Nav->getButtonPress(CROSS) && !PS3Nav->getButtonPress(CIRCLE) && !PS3Nav->getButtonPress(L1) && !PS3Nav->getButtonPress(L2) && !PS3Nav->getButtonPress(PS)) {
+	myDFPlayer.play(2);
 	}
 	// Left - 
 	if (PS3Nav->getButtonClick(LEFT) && !PS3Nav->getButtonPress(CROSS) && !PS3Nav->getButtonPress(CIRCLE) && !PS3Nav->getButtonPress(L1) && !PS3Nav->getButtonPress(L2) && !PS3Nav->getButtonPress(PS)) {
+	myDFPlayer.play(4);
 	}
 	// Right - 
 	if (PS3Nav->getButtonClick(RIGHT) && !PS3Nav->getButtonPress(CROSS) && !PS3Nav->getButtonPress(CIRCLE) && !PS3Nav->getButtonPress(L1) && !PS3Nav->getButtonPress(L2) && !PS3Nav->getButtonPress(PS)) {
+	myDFPlayer.play(7);
 	}
 }
 
 // =======================================================================================
-// Program Utility Functions - Called from various locations
+// Functions - Called from various locations
 // =======================================================================================
 
 void toggleSettings()
@@ -966,6 +1001,249 @@ void footServoDetach()
 	   }
     }
 }
+
+void animationSounds()     // to be replaced with routine
+{
+	soundcurrentmillis = millis();  // update timer
+	soundTimer = soundcurrentmillis - soundprevmillis;
+	if (soundTimer > 15000 && soundNum == 1)
+	{
+		myDFPlayer.play(4);
+		soundNum =2;
+	}
+	if (soundTimer > 35000 && soundNum == 2)
+	{ 
+		myDFPlayer.play(13);
+		soundNum =3;
+	}
+	if (soundTimer > 60000 && soundNum == 3)
+	{ 
+		myDFPlayer.play(1);
+		soundNum =4;
+	}
+	if (soundTimer > 85000 && soundNum == 4)
+	{ 
+		myDFPlayer.play(3);
+		soundNum =5;
+	}
+	if (soundTimer > 115000 && soundNum == 5)
+	{ 
+		myDFPlayer.play(14);
+		soundNum =6;
+	}
+	if (soundTimer > 145000 && soundNum == 6)
+	{ 
+		myDFPlayer.play(9);
+		soundNum =7;
+	}
+	if (soundTimer > 185000 && soundNum == 7)
+	{
+		myDFPlayer.play(11);
+		soundprevmillis = soundcurrentmillis;
+		soundNum =1;
+	}
+	    Serial.println("Animation Sound: ");
+		Serial.println(soundNum);
+}
+
+void animationLow()  // to be replaced with routines - idea is to create building blocks and call them here in order
+{
+   // if not expanded to full, expand to full
+   // if not head raised, raise head
+   // look left
+   // look forward
+   // look left slowly
+   // look forward
+   // look right
+   // look all the way right slowly
+   // back to center slowly
+   // tilt head around
+   // say casa
+   // look down
+   // look up
+   // level head
+   // tuck head in
+   // wait a bit
+   // start loop again
+   
+   //Serial.println("Animation Low");
+}
+
+void animationHigh()   // to be replaced with routines - idea is to create building blocks and call them here in order
+{
+	// if not expanded to full, expand to full
+	// if not head raised, raise head
+	// timer - look left
+	// look more left slowly
+	// slowly back to center
+	// say something
+	// timer - look right
+	// time  - tilt head some direction
+	// timer - look more right
+	// time  - tilt head more direction
+	// timer - look forward
+	// spin rings and make a sound
+	// timer - lower a half
+	// stop spinning rings
+	// say something else
+	// expand up
+	// look left then right, then left, right, forward
+	// say something
+	// tilt backward and look forward and up
+	// say casa, casa, i miss marva
+	// wait a short while then tuck in
+    animationRelaxed(); // relax
+	// wait a while
+	// start loop again
+
+	//Serial.println("Animation High");
+}
+
+void centerHead()
+{
+	if (HeadT < HeadTCenter) { 
+		for (int i=HeadT; i<HeadTCenter; i++) {
+		ServoHeadT.writeMicroseconds(i);   // loop HeadT to to center
+		}
+	} else {
+		for (int i=HeadT; HeadTCenter<i; i--) {
+		ServoHeadT.writeMicroseconds(i);   // loop HeadT to center
+		}
+	}
+	HeadT = HeadTCenter;
+	ServoHeadT.writeMicroseconds(HeadT);
+}
+
+void levelHead()
+{
+	if (HeadL < HeadLCenter) { 
+		for (int i=HeadL; i<HeadLCenter; i++) {
+		ServoHeadL.writeMicroseconds(i);   // loop HeadT to to center
+		}
+	} else {
+		for (int i=HeadL; HeadLCenter<i; i--) {
+		ServoHeadL.writeMicroseconds(i);   // loop HeadT to center
+		}
+	}
+	HeadL = HeadLCenter;
+	ServoHeadL.writeMicroseconds(HeadL);
+
+	if (HeadR < HeadRCenter) { 
+		for (int i=HeadR; i<HeadRCenter; i++) {
+		ServoHeadR.writeMicroseconds(i);   // loop HeadT to to center
+		}
+	} else {
+		for (int i=HeadR; HeadRCenter<i; i--) {
+		ServoHeadR.writeMicroseconds(i);   // loop HeadT to center
+		}
+	}
+	HeadR = HeadRCenter;
+	ServoHeadR.writeMicroseconds(HeadR);
+}
+
+void raiseHead()
+{
+	for (int i=HeadZ; HeadZMin<i; i--) {
+	ServoHeadZ.writeMicroseconds(i);   // loop head Z to to min value (opened)
+	}
+	HeadZ = HeadZMin;
+}
+
+void lowerHead()
+{
+	for (int i=HeadZ; i<HeadZMax; i++) {
+	ServoHeadZ.writeMicroseconds(i);   // loop head Z to to max value (closed)
+	}
+	HeadZ = HeadZMax;
+}
+
+
+void tuckInBody()   // to be replaced with routine
+{
+    Serial.println("Tuck In");
+	myDFPlayer.play(11);
+	for (int i=BodyZ2; i<BodyZ2Max; i++) {
+	ServoBodyZ2.writeMicroseconds(i);   // loop body z2 to to max value (closed)
+	}
+	for (int i=BodyZ1; BodyZ1Min<i; i--) {
+	ServoBodyZ1.writeMicroseconds(i);   // loop body z1 to min (closed)
+	}
+	BodyZ1 = BodyZ1Min;
+	BodyZ2 = BodyZ2Max;
+}
+
+void expandBody()   // to be replaced with routine
+{
+	Serial.println("Expand");
+	for (int i=BodyZ1; i<BodyZ1Max; i++) {
+	ServoBodyZ1.writeMicroseconds(i);   // loop body z2 to to max value (opened)
+    }
+	for (int i=BodyZ2; BodyZ2Min<i; i--) {
+	ServoBodyZ2.writeMicroseconds(i);   // loop body z1 to min (opened)
+	}
+	BodyZ1 = BodyZ1Max;
+	BodyZ2 = BodyZ2Min;
+	myDFPlayer.play(11);    
+}
+
+void animationRelaxed()   // Move all positions to the middle of the extents
+{
+	Drive = 1500;
+	Rotate = 1500;
+	Strafe = 1500;
+	HeadL = HeadLCenter;
+	HeadR = HeadRCenter;
+	HeadT = HeadTCenter;
+	HeadZ = (HeadZMin + HeadZMax)/2;
+	BodyZ1 = (BodyZ1Min + BodyZ1Max)/2;
+	BodyZ2 = (BodyZ2Min + BodyZ2Max)/2;
+	FLSpread = (FLSpreadMin + FLSpreadMax)/2;
+	FRSpread = (FRSpreadMin + FRSpreadMax)/2;
+	BRSpread = (BRSpreadMin + BRSpreadMax)/2;
+	BLSpread = (BLSpreadMin + BLSpreadMax)/2;
+	FLHeight = (FLHeightMin + FLHeightMax)/2;
+	FRHeight = (FRHeightMin + FRHeightMax)/2;
+	BRHeight = (BRHeightMin + BRHeightMax)/2;
+	BLHeight = (BLHeightMin + BLHeightMax)/2;
+	ServoFLHeight.attach(PinFLHeight);
+	ServoFRHeight.attach(PinFRHeight);
+	ServoBRHeight.attach(PinBRHeight);
+	ServoBLHeight.attach(PinBLHeight);
+	ServoFLSpread.attach(PinFLSpread);
+	ServoFRSpread.attach(PinFRSpread);
+	ServoBRSpread.attach(PinBRSpread);
+	ServoBLSpread.attach(PinBLSpread);
+	ServoDrive.writeMicroseconds(Drive);
+	ServoRotate.writeMicroseconds(Rotate);
+	ServoStrafe.writeMicroseconds(Strafe);
+	ServoHeadL.writeMicroseconds(HeadL);
+	ServoHeadR.writeMicroseconds(HeadR);
+	ServoHeadZ.writeMicroseconds(HeadZ);
+	ServoHeadT.writeMicroseconds(HeadT);
+	ServoBodyZ1.writeMicroseconds(BodyZ1);
+	ServoBodyZ2.writeMicroseconds(BodyZ2);
+	ServoFLSpread.writeMicroseconds(FLSpread);
+	ServoFRSpread.writeMicroseconds(FRSpread);
+	ServoBRSpread.writeMicroseconds(BRSpread);
+	ServoBLSpread.writeMicroseconds(BLSpread);
+	ServoFLHeight.writeMicroseconds(FLHeight);
+	ServoFRHeight.writeMicroseconds(FRHeight);
+	ServoBRHeight.writeMicroseconds(BRHeight);
+	ServoBLHeight.writeMicroseconds(BLHeight);
+	delay(1500);
+	ServoFLSpread.detach();
+   	ServoFRSpread.detach();
+   	ServoBRSpread.detach();
+   	ServoBLSpread.detach();
+	ServoFLHeight.detach();
+   	ServoFRHeight.detach();
+   	ServoBRHeight.detach();
+   	ServoBLHeight.detach();
+	footServosAttached = false;
+	Serial.println("Relaxed");
+}
+
+
 
 
 // =======================================================================================
